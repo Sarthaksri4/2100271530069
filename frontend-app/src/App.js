4e6c436d3a532139c6a43acb4124ef87f0a6355e
sarthak <sarthaksri044@gmail.com>
2024-06-08 12:51:05 +0530

    changes

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        E-Commerce App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+          
+        </Router>
+    );
 }
 
 export default App;
